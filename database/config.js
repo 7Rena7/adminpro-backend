4f9c08e92965cac0336c4d5400bb82a7c92b2c0e
Renato Corbellini <renacorbe@gmail.com>
2023-11-27 16:50:20 -0300

    Update config.js

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://renato:RGMfbkE3sv9OkxQE@cluster0.yvzfiab.mongodb.net/"
+      ""
     );
     console.log("DB Online");
   } catch (error) {
